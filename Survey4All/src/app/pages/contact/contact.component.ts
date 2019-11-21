9d606ef258c701e72bbbed24b72eba692e0c95e5
umutyildiz <umut.yildiz05@hotmail.com>
2019-11-21 23:26:40 +0300

    update contact us

--- Survey4All/src/app/pages/contact/contact.component.ts
+++ Survey4All/src/app/pages/contact/contact.component.ts
@@ -8,13 +8,13 @@ import { FirebaseService } from 'src/app/services/firebase/firebase.service';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-
+    submitClick:boolean=true;
   constructor(private fbService: FirebaseService) { }
 
   ngOnInit() {
   }
   contact(name:string,email:string,subject:string,message:string){
     this.fbService.contact(name,email,subject,message);
-
+    this.submitClick=false;
   }
 }
