2ae724a23c4ab029701e1993f0cbaa5047b10ea7
gozdegok <gozde.gokyokus@gmail.com>
2019-12-10 16:45:12 +0300

    view survey page under construction

--- Survey4All/src/app/view-survey/view-survey.component.ts
+++ Survey4All/src/app/view-survey/view-survey.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from '../services/auth/auth.service';
+import { snapshotChanges } from '@angular/fire/database';
 
 @Component({
   selector: 'app-view-survey',
@@ -9,17 +10,46 @@ import { AuthService } from '../services/auth/auth.service';
 })
 export class ViewSurveyComponent implements OnInit {
 
-  qTitle = "";
-  qType = "";
+  qTitles = [];
+  qTypes = [];
   isMultiple = false;
   answerList = [];
   qList = [];
-  surveyName="";
-  email="";
-  constructor(private db:AngularFirestore, private authService:AuthService ) {
+  surveyName = "";
+  email = "";
+
+  constructor(private db: AngularFirestore, private auth: AuthService) {
+
+    db.collection("surveys").doc("deneme2@deneme.com").get().subscribe(resp => {
+      var mySurveys = resp.data()["mySurveys"];
+      this.qList = mySurveys[0].qList;
+      this.surveyName = mySurveys[0].surveyName;
+      this.qList = mySurveys.qList;
+      console.log(this.qList)
+      this.answerList = this.qList[0].answerList;
+      this.qList.forEach(question => {
+        var question = question.data();
+        var qType = question.data()["qType"];
+this.qTypes.push(qType);
+var qTitle= question.data()["qTitle"];
+this.qTitles.push(qTitle);
+console.log(this.qTitles[0])
+});
+
+    });
+
+
   }
+
+
+
+
   ngOnInit() {
   }
 
 
+
+
+
 }
+
